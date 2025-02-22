ab69243b5637d72ca85cb31a254e2fda554205a7
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2025-02-22 20:06:29 +0900

    feat : Header

--- src/components/layout/home.tsx
+++ src/components/layout/home.tsx
@@ -16,7 +16,7 @@ const Home: FC<{
                 <div className="w-full h-full flex flex-row">
                     <Sidebar />
                     <div className="w-full h-full flex flex-col">
-                        {/* <Header /> */}
+                        <Header />
                         <div className="w-full flex-1 basis-0 overflow-y-auto">{children}</div>
                     </div>
                 </div>
